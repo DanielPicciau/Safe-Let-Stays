5ae109542aacfa4e599344231128bf0479f5e3c1
WebFlare UK <webflareuk@gmail.com>
2025-12-08 00:45:33 +0000

    Explicitly load .env file in settings.py

--- safeletstays/settings.py
+++ safeletstays/settings.py
@@ -7,9 +7,11 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
-
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load .env file explicitly
+load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = 'dev-secret-key-change-in-production'
 
