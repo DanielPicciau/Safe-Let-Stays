07cdd2b0f51a40ff6690488b021abc3af7d4ff38
WebFlare UK <danieljunior.business@gmail.com>
2026-01-17 14:58:59 +0000

    Allow unpkg.com in CSP for React

--- yourapp/security.py
+++ yourapp/security.py
@@ -157,12 +157,12 @@ class SecurityHeadersMiddleware(MiddlewareMixin):
         # Content Security Policy
         csp_directives = [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://fonts.googleapis.com",
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://fonts.googleapis.com https://unpkg.com",
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
             "font-src 'self' https://fonts.gstatic.com data:",
             "img-src 'self' data: https: blob:",
             "frame-src 'self' https://js.stripe.com https://hooks.stripe.com",
-            "connect-src 'self' https://api.stripe.com https://fonts.googleapis.com",
+            "connect-src 'self' https://api.stripe.com https://fonts.googleapis.com https://unpkg.com",
             "base-uri 'self'",
             "form-action 'self' https://checkout.stripe.com",
             "frame-ancestors 'none'",
